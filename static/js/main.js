27c5e4c7669f5086ac0001d430244c29c95c87f2
yukito1218 <132879582+yukito1218@users.noreply.github.com>
2023-06-12 15:43:04 +0900

    写真表示

--- static/js/main.js
+++ static/js/main.js
@@ -3,20 +3,35 @@ document.addEventListener("DOMContentLoaded", function () {
   var messageInput = document.getElementsByClassName("message-input")[0];
   var messageList = document.getElementsByClassName("message-list")[0];
 
-  sendButton.addEventListener("click", function () {
+  function sendMessage() {
     var messageText = messageInput.value.trim();
 
     if (messageText !== "") {
       var messageDiv = document.createElement("div");
       messageDiv.className = "message";
 
+      var senderImage = document.createElement("img");
+      senderImage.src = "https://theoita.com/wp-content/uploads/2019/09/00111217_008-1.jpg";
+      senderImage.alt = "User1";
+
+      var senderPhotoDiv = document.createElement("div");
+      senderPhotoDiv.className = "sender-photo";
+      senderPhotoDiv.appendChild(senderImage);
+
       var senderDiv = document.createElement("div");
-      senderDiv.className = "sender";
-      senderDiv.textContent = "User1:";
+      senderDiv.className = "sender right";
+      senderDiv.appendChild(senderPhotoDiv);
 
       var contentDiv = document.createElement("div");
       contentDiv.className = "content";
       contentDiv.textContent = messageText;
+
+      var sameSenderMessages = messageList.getElementsByClassName("same-sender");
+      if (sameSenderMessages.length > 0) {
+        var lastSameSenderMessage = sameSenderMessages[sameSenderMessages.length - 1];
+        var senderDiv = lastSameSenderMessage.querySelector(".sender");
+        senderDiv.style.display = "none";
+      }
 
       messageDiv.appendChild(senderDiv);
       messageDiv.appendChild(contentDiv);
@@ -24,6 +39,19 @@ document.addEventListener("DOMContentLoaded", function () {
       messageInput.value = "";
       messageList.scrollTop = messageList.scrollHeight;
     }
+  }
+
+  messageInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      sendMessage();
+    }
+  });
+
+  sendButton.addEventListener("click", function () {
+    sendMessage();
+
+    var messageText = messageInput.value.trim();
 
     fetch("/get_variable", {
       method: "POST",
@@ -37,9 +65,17 @@ document.addEventListener("DOMContentLoaded", function () {
         var messageDiv = document.createElement("div");
         messageDiv.className = "answer";
 
+        var senderImage = document.createElement("img");
+        senderImage.src = "https://example.com/user2.jpg"; // User2の画像のURLを設定
+        senderImage.alt = "User2";
+
+        var senderPhotoDiv = document.createElement("div");
+        senderPhotoDiv.className = "sender-photo";
+        senderPhotoDiv.appendChild(senderImage);
+
         var senderDiv = document.createElement("div");
-        senderDiv.className = "sender";
-        senderDiv.textContent = "User2:";
+        senderDiv.className = "sender left";
+        senderDiv.appendChild(senderPhotoDiv);
 
         var contentDiv = document.createElement("div");
         contentDiv.className = "content";
